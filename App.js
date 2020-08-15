ec9e82a1d8da65eab44edc74cdb32c8848af7ef4
faisalashraf44 <faisalashraf786.fa@gmail.com>
2020-08-15 11:58:10 +0500

    feat(Notifications): working on push notifications setup

--- App.js
+++ App.js
@@ -2,11 +2,54 @@ import React, {Component} from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
 import Navigation from './src/Navigation/Navigation';
 import {RootContext} from './src/Backend/Context';
+import {fcmService} from './src/FCMService';
+import {localNotificationService} from './src/LocalNotificationService';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  onRegister = token => {
+    console.log('[App] onRegister: ', token);
+  };
+
+  onNotification = notify => {
+    console.log('[App] onNotification: ', notify);
+    const options = {
+      soundName: 'default',
+      playSound: true,
+    };
+
+    localNotificationService.showNotification(
+      0,
+      notify.title,
+      notify.body,
+      notify,
+      options,
+    );
+  };
+
+  onOpenNotification = notify => {
+    console.log('[App] onOpenNotification: ', notify);
+    alert('Open Notification: ' + notify.body);
+  };
+
+  async componentDidMount() {
+    fcmService.registerAppWithFCM();
+    fcmService.register(
+      this.onRegister,
+      this.onNotification,
+      this.onOpenNotification,
+    );
+    localNotificationService.configure(this.onOpenNotification);
+  }
+
+  async componentWillUnmount() {
+    console.log('[App] unregister');
+    fcmService.unRegister();
+    localNotificationService.unregister();
   }
 
   render() {
